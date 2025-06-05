a0a04aed6824bfe206ca1341f800ed07feee561c
cesargvna <cesargvna@gmail.com>
2025-06-04 20:29:27 -0400

    refactor: enhance token handling by adding expiration time and improving token signing logic

--- src/utils/handleToken.ts
+++ src/utils/handleToken.ts
@@ -4,6 +4,7 @@ import  {SECRET} from './config';
 if (!SECRET) {
     console.error("Error: Missing JWT_SECRET. Token generation will fail.");
 }
+const TOKEN_DURATION_MS = 1000 * 60 * 60;
 const tokenSign = async (user: any): Promise<string> => {
     try {
       const userForToken = {
@@ -11,11 +12,14 @@ const tokenSign = async (user: any): Promise<string> => {
         id: user.id,
       };
       
-      return jwt.sign(userForToken, SECRET, { expiresIn: 60 * 60 });
+      return jwt.sign(userForToken, SECRET, { expiresIn: TOKEN_DURATION_MS / 1000 });
     } catch (error) {
       console.error("Error signing JWT:", error);
       throw new Error("Token generation failed");
     }
 };
+const getTokenExpireTime = (): number => {
+  return Date.now() + TOKEN_DURATION_MS;
+};
 
-export {tokenSign};
+export {tokenSign,getTokenExpireTime};
